9120683f4aa7cbf84f68a812d8ccef9a933e5992
leepsyko <letho161299@gmail.com>
2023-11-06 09:53:10 +0700

    tei len user context và chưa css cho  sign

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import ClientLayout from "./layouts/ClientLayout/ClientLayout";
 import Home from "./module/home/Home";
 import RoomList from "./module/roomlist/RoomList";
 import RoomDetails from "./module/roomdetails/RoomDetails";
+import SignIn from "./module/Auth/SignIn";
+import UserProvider from "./context/UserContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ClientLayout />}>
-          <Route index element={<Home />} />
-          <Route path="roomlist/:locationId" element={<RoomList />} />
-          <Route path="roomdetails/:roomId" element={<RoomDetails />} />
-        </Route>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ClientLayout />}>
+            <Route index element={<Home />} />
+            <Route path="roomlist/:locationId" element={<RoomList />} />
+            <Route path="roomdetails/:roomId" element={<RoomDetails />} />
 
-        <Route path="*" element={<div>NOT FOUND</div>} />
-      </Routes>
-    </BrowserRouter>
+            <Route path="/sign-in" element={<SignIn />} />
+          </Route>
+
+          <Route path="*" element={<div>NOT FOUND</div>} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,6 +12,7 @@ import {
   List,
   ListItemButton,
   TextField,
+  Modal,
 } from "@mui/material";
 import { styled } from "@mui/system";
 import { colorConfigs } from "../../configs/colorConfigs";
@@ -23,6 +24,11 @@ import LanguageIcon from "@mui/icons-material/Language";
 import DatePicker from "../DatePicker/DatePicker";
 import { useQuery } from "@tanstack/react-query";
 import { getLocation } from "../../apis/positionAPI";
+import SignUp from "../../module/Auth/SignUp/SignUp";
+import SignIn from "../../module/Auth/SignIn/SignIn";
+import { useUserContext } from "../../context/UserContext";
+
+
 
 const Logo = styled("a")`
   display: flex;
@@ -92,11 +98,51 @@ const StyledBox = styled(Box)`
   cursor: pointer;
 `;
 
+const styleSign = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+
 export default function Header() {
   const [anchorEl, setAnchorEl] = useState(null);
   const [selectedButton, setSelectedButton] = useState(null);
   const [focusedBox, setFocusedBox] = useState(null);
   const open = Boolean(anchorEl);
+
+
+  //check user form context 
+  const { currentUser, handleSignOutContext } = useUserContext();
+
+
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const handleOpenSignIn = () => {
+    setOpenSignIn(true)
+  };
+  const handleCloseSignIn = () => { setOpenSignIn(false) };
+
+
+
+  const [openSignUp, setOpenSignUp] = useState(false);
+  const handleOpenSignUp = () => {
+    setOpenSignUp(true)
+  };
+  const handleCloseSignUp = () => {
+
+    console.log("dã chạy")
+    setOpenSignUp(false)
+  };
+
+
+
 
   const { data: location = [] } = useQuery({
     queryKey: ["location"],
@@ -126,202 +172,244 @@ export default function Header() {
     }
   };
 
+
+
+  // Kiểm tra user có đăng nhập hay chưa
+
+  if(currentUser !== null){
+    console.log("có đăng nhâp")
+  }
+
+
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" variant="elevation" color="inherit">
-        <Toolbar sx={{ transition: "0.3s all" }}>
+
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" variant="elevation" color="inherit">
+          <Toolbar sx={{ transition: "0.3s all" }}>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "10px 0px",
+              }}
+            >
+              {/* LOGO */}
+              <Logo>
+                <Typography
+                  sx={{ display: "block" }}
+                  component="img"
+                  width={100}
+                  src={AirBnb}
+                  alt="AirBnb"
+                />
+              </Logo>
+              {/* List Buttons*/}
+              <Box sx={{ m: "auto" }} component="div">
+                <List sx={{ display: "flex", gap: "10px" }}>
+                  <ListButton
+                    disableTouchRipple
+                    onClick={() => handleSelectedButton("item1")}
+                    selected={selectedButton === "item1"}
+                  >
+                    Chỗ Ở
+                  </ListButton>
+                  <ListButton
+                    disableTouchRipple
+                    onClick={() => handleSelectedButton("item2")}
+                    selected={selectedButton === "item2"}
+                  >
+                    Trải Nghiệm
+                  </ListButton>
+                  <ListButton
+                    disableTouchRipple
+                    onClick={() => handleSelectedButton("item3")}
+                    selected={selectedButton === "item3"}
+                  >
+                    Trải Nghiệm Trực Tuyến
+                  </ListButton>
+                </List>
+              </Box>
+              {/*Choose Language Button */}
+              <IconButton>
+                <LanguageIcon />
+              </IconButton>
+              {/* Signin/Signup Button */}
+              <MainButton
+                onClick={handleOpenMenu}
+                color="inherit"
+                variant="outlined"
+                aria-label="sing-in-sign-up-button"
+                startIcon={<MenuIcon />}
+                endIcon={
+                  <AccountCircleIcon sx={{ fontSize: "40px !important" }} />
+                }
+              />
+            </Box>
+          </Toolbar>
+
+          {/* Search System */}
           <Box
             sx={{
-              flexGrow: 1,
+              bgcolor: focusedBox !== null ? "lightgrey" : "",
               display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              padding: "10px 0px",
+              borderRadius: "50px",
+              border: "1px solid lightgrey",
+              m: "auto",
+              marginBottom: "10px",
+              transition: "0.3s ease-in-out",
             }}
-          >
-            {/* LOGO */}
-            <Logo>
-              <Typography
-                sx={{ display: "block" }}
-                component="img"
-                width={100}
-                src={AirBnb}
-                alt="AirBnb"
-              />
-            </Logo>
-            {/* List Buttons*/}
-            <Box sx={{ m: "auto" }} component="div">
-              <List sx={{ display: "flex", gap: "10px" }}>
-                <ListButton
-                  disableTouchRipple
-                  onClick={() => handleSelectedButton("item1")}
-                  selected={selectedButton === "item1"}
-                >
-                  Chỗ Ở
-                </ListButton>
-                <ListButton
-                  disableTouchRipple
-                  onClick={() => handleSelectedButton("item2")}
-                  selected={selectedButton === "item2"}
-                >
-                  Trải Nghiệm
-                </ListButton>
-                <ListButton
-                  disableTouchRipple
-                  onClick={() => handleSelectedButton("item3")}
-                  selected={selectedButton === "item3"}
-                >
-                  Trải Nghiệm Trực Tuyến
-                </ListButton>
-              </List>
-            </Box>
-            {/*Choose Language Button */}
-            <IconButton>
-              <LanguageIcon />
-            </IconButton>
-            {/* Signin/Signup Button */}
-            <MainButton
-              onClick={handleOpenMenu}
-              color="inherit"
-              variant="outlined"
-              aria-label="sing-in-sign-up-button"
-              startIcon={<MenuIcon />}
-              endIcon={
-                <AccountCircleIcon sx={{ fontSize: "40px !important" }} />
-              }
-            />
-          </Box>
-        </Toolbar>
-
-        {/* Search System */}
-        <Box
-          sx={{
-            bgcolor: focusedBox !== null ? "lightgrey" : "",
-            display: "flex",
-            borderRadius: "50px",
-            border: "1px solid lightgrey",
-            m: "auto",
-            marginBottom: "10px",
-            transition: "0.3s ease-in-out",
-          }}
-          autoComplete="off"
-          component="div"
-        >
-          {/* Find Location Part */}
-          <StyledBox
-            sx={{
-              bgcolor: focusedBox === 0 ? "white" : "",
-              borderRadius: focusedBox === 0 ? "32px" : "",
-              boxShadow:
-                focusedBox === 0 ? ` rgba(0, 0, 0, 0.35) 0px 5px 15px` : "",
-            }}
+            autoComplete="off"
             component="div"
-            onFocus={() => handleBoxFocus(0)}
-            onBlur={handleBoxBlur}
           >
-            <Typography variant="subtitle2">Địa điểm</Typography>
-            <StyledTextField
-              variant="standard"
-              type="search"
-              placeholder="Tìm địa điểm đến"
-              inputProps={{
-                style: {
-                  border: "none",
-                },
+            {/* Find Location Part */}
+            <StyledBox
+              sx={{
+                bgcolor: focusedBox === 0 ? "white" : "",
+                borderRadius: focusedBox === 0 ? "32px" : "",
+                boxShadow:
+                  focusedBox === 0 ? ` rgba(0, 0, 0, 0.35) 0px 5px 15px` : "",
               }}
-            />
-          </StyledBox>
-          {/* Start Date Part */}
-          <StyledBox
-            sx={{
-              bgcolor: focusedBox === 1 ? "white" : "",
-              borderRadius: focusedBox === 1 ? "32px" : "",
-              boxShadow:
-                focusedBox === 1 ? ` rgba(0, 0, 0, 0.35) 0px 5px 15px` : "",
-            }}
-            component="div"
-            onFocus={() => handleBoxFocus(1)}
-            onBlur={handleBoxBlur}
-          >
-            <Typography variant="subtitle2">Nhận phòng</Typography>
-            <DatePicker variant="standard" type="date" />
-          </StyledBox>
-          {/* End Date Part */}
-          <StyledBox
-            sx={{
-              bgcolor: focusedBox === 2 ? "white" : "",
-              borderRadius: focusedBox === 2 ? "32px" : "",
-              boxShadow:
-                focusedBox === 2 ? ` rgba(0, 0, 0, 0.35) 0px 5px 15px` : "",
-            }}
-            component="div"
-            onFocus={() => handleBoxFocus(2)}
-            onBlur={handleBoxBlur}
-          >
-            <Typography variant="subtitle2">Trả phòng</Typography>
-            <DatePicker variant="standard" type="date" />
-          </StyledBox>
-          {/* Guest Part */}
-          <StyledBox
-            sx={{
-              display: "flex",
-              gap: "5px",
-              bgcolor: focusedBox === 3 ? "white" : "",
-              borderRadius: focusedBox === 3 ? "32px" : "",
-              boxShadow:
-                focusedBox === 3 ? ` rgba(0, 0, 0, 0.35) 0px 5px 15px` : "",
-            }}
-            component="div"
-            onFocus={() => handleBoxFocus(3)}
-            onBlur={handleBoxBlur}
-          >
-            <Box component="div">
-              <Typography variant="subtitle2">Khách</Typography>
-              <Typography variant="subtitle2">0</Typography>
-            </Box>
-            <SearchButton variant="contained">
-              <SearchIcon />
-            </SearchButton>
-          </StyledBox>
-        </Box>
+              component="div"
+              onFocus={() => handleBoxFocus(0)}
+              onBlur={handleBoxBlur}
+            >
+              <Typography variant="subtitle2">Địa điểm</Typography>
+              <StyledTextField
+                variant="standard"
+                type="search"
+                placeholder="Tìm địa điểm đến"
+                inputProps={{
+                  style: {
+                    border: "none",
+                  },
+                }}
+              />
+            </StyledBox>
+            {/* Start Date Part */}
+            <StyledBox
+              sx={{
+                bgcolor: focusedBox === 1 ? "white" : "",
+                borderRadius: focusedBox === 1 ? "32px" : "",
+                boxShadow:
+                  focusedBox === 1 ? ` rgba(0, 0, 0, 0.35) 0px 5px 15px` : "",
+              }}
+              component="div"
+              onFocus={() => handleBoxFocus(1)}
+              onBlur={handleBoxBlur}
+            >
+              <Typography variant="subtitle2">Nhận phòng</Typography>
+              <DatePicker variant="standard" type="date" />
+            </StyledBox>
+            {/* End Date Part */}
+            <StyledBox
+              sx={{
+                bgcolor: focusedBox === 2 ? "white" : "",
+                borderRadius: focusedBox === 2 ? "32px" : "",
+                boxShadow:
+                  focusedBox === 2 ? ` rgba(0, 0, 0, 0.35) 0px 5px 15px` : "",
+              }}
+              component="div"
+              onFocus={() => handleBoxFocus(2)}
+              onBlur={handleBoxBlur}
+            >
+              <Typography variant="subtitle2">Trả phòng</Typography>
+              <DatePicker variant="standard" type="date" />
+            </StyledBox>
+            {/* Guest Part */}
+            <StyledBox
+              sx={{
+                display: "flex",
+                gap: "5px",
+                bgcolor: focusedBox === 3 ? "white" : "",
+                borderRadius: focusedBox === 3 ? "32px" : "",
+                boxShadow:
+                  focusedBox === 3 ? ` rgba(0, 0, 0, 0.35) 0px 5px 15px` : "",
+              }}
+              component="div"
+              onFocus={() => handleBoxFocus(3)}
+              onBlur={handleBoxBlur}
+            >
+              <Box component="div">
+                <Typography variant="subtitle2">Khách</Typography>
+                <Typography variant="subtitle2">0</Typography>
+              </Box>
+              <SearchButton variant="contained">
+                <SearchIcon />
+              </SearchButton>
+            </StyledBox>
+          </Box>
 
-        {/* Menu open when onclick by MainButton */}
-        <Menu
-          slotProps={{
-            paper: {
-              sx: {
-                overflow: "visible",
-                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                mt: 1,
-                "&:before": {
-                  content: '""',
-                  display: "block",
-                  position: "absolute",
-                  top: 0,
-                  right: 14,
-                  width: 10,
-                  height: 10,
-                  bgcolor: "background.paper",
-                  transform: "translateY(-50%) rotate(45deg)",
-                  zIndex: 0,
+          {/* Menu open when onclick by MainButton */}
+          <Menu
+            slotProps={{
+              paper: {
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1,
+                  "&:before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
+                  },
                 },
               },
-            },
-          }}
-          transformOrigin={{ horizontal: "right", vertical: "top" }}
-          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-          open={open}
-          anchorEl={anchorEl}
-          onClose={handleCloseMenu}
-          onClick={handleCloseMenu}
-        >
-          <MenuItem>Đăng Nhập</MenuItem>
-          <MenuItem>Đăng Ký</MenuItem>
-          <Divider />
-          <MenuItem>Cho thuê chỗ ở qua AirBnb</MenuItem>
-          <MenuItem>Trung tâm trợ giúp</MenuItem>
-        </Menu>
-      </AppBar>
-    </Box>
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleCloseMenu}
+            onClick={handleCloseMenu}
+          >
+            <MenuItem onClick={handleOpenSignIn}>Đăng Nhập</MenuItem>
+            <MenuItem onClick={handleOpenSignUp}>Đăng Ký</MenuItem>
+            <Divider />
+            <MenuItem>Cho thuê chỗ ở qua AirBnb</MenuItem>
+            <MenuItem>Trung tâm trợ giúp</MenuItem>
+          </Menu>
+        </AppBar>
+      </Box>
+
+
+
+
+      {/* Modal Sign In user  */}
+
+      <Modal
+        open={openSignIn}
+        onClose={handleCloseSignIn}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <div>
+
+          <SignIn />
+        </div>
+      </Modal>
+
+      {/* Modal Sign Up user  */}
+      <Modal
+        open={openSignUp}
+        onClose={handleCloseSignUp}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <div>
+          <SignUp handleCloseSignUp={handleCloseSignUp} handleOpenSignIn={handleOpenSignIn} />
+        </div>
+      </Modal>
+    </>
   );
 }
